2950d62b4a0778dd8a165f947730699fabfb4ed3
Sergey <sergey.riedel@gmail.com>
2017-03-21 21:25:09 +0100

    Completed laying out header-clients sections

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require("gulp"),
+    scss = require("gulp-sass"),
+    browserSync = require("browser-sync"),
+    autoprefixer = require("autoprefixer"),
+    plumber = ("gulp-plumber");
+
+
+// Sass compilation
+gulp.task("scss", function() {
+    return gulp.src("src/scss/**/*.scss")
+        // .pipe(plumber())
+        .pipe(scss(autoprefixer))
+        .pipe(gulp.dest("src/css/"))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+// Browser-Sync live reload
+gulp.task("browser-sync", function() {
+    browserSync({
+        server: {
+            baseDir: "./src/"
+        }
+    });
+});
+
+// Watch task
+gulp.task("watch", ["browser-sync", "scss"], function() {
+    gulp.watch("src/scss/**/*.scss", ["scss"]);
+    gulp.watch("src/**/*.html", browserSync.reload);
+    gulp.watch("src/js/**/*.js", browserSync.reload);
+});
